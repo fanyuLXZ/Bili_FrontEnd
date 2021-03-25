7f8f22400f7ce20b97ae4032c04d6ce84db5e7f5
fanyuLXZ <1554296667@qq.com>
2021-03-25 23:18:05 +0800

    增加css重置样式库

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+//CSS 重置样式库
+import "./assets/normalize.css"
+
 createApp(App).use(store).use(router).mount('#app')
