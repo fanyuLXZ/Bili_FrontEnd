aaaf29dda9bb8a1030721673b0e5e9eb2f7405c4
fanyuLXZ <1554296667@qq.com>
2021-03-27 17:33:57 +0800

    新增bili-tooltip组件

---
+++ src/components/others/bili-tooltip.vue
@@ -0,0 +1,32 @@
+<!--作者:李信志-->
+<template>
+  <div class="bili-tooltip" v-show="true">
+    <div :class="'bili-tooltip-container '+ shadow_class">
+      <slot name="container"></slot>
+    </div>
+    <div :class="'bili-tooltip-arrow '+arrow_position"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bili-tooltip",
+  props: ["position", "is_show", "shadow"],
+  computed: {
+    arrow_position(){
+      if (this.position!==''&&this.position!==undefined){
+        return 'bili-tooltip-arrow-'+this.position
+      }
+      return ''
+    },
+    shadow_class(){
+      let shadow_class_dic={
+        basic:"box-shadow-basic",
+        shallow:"box-shadow-shallow"
+      }
+      let shadow_class_value = shadow_class_dic[this.shadow]
+      return shadow_class_value!==undefined?shadow_class_value:"box-shadow-basic"
+    }
+  }
+}
+</script>
