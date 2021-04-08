3ab83d6ab32c03b804f0a923649f741a3fd3c49c
fanyuLXZ <1554296667@qq.com>
2021-04-08 22:19:50 +0800

    初始化动态，消息，空间模块

---
+++ public/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <international-header :nav-type="1"/>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Image, Danmu, Framepreview, Watchlater, Slide, Lazyload, Backtop, Report } from 'g-public/modules/@bilibili/van'
+
+import InternationalHeader from "./components/international-header";
+
+Vue.use(Image)
+Vue.use(Danmu)
+Vue.use(Framepreview)
+Vue.use(Watchlater)
+Vue.use(Slide)
+Vue.use(Lazyload, { preload: 100 })
+Vue.use(Backtop)
+Vue.use(Report)
+
+export default {
+  components: {InternationalHeader,}
+}
+</script>
