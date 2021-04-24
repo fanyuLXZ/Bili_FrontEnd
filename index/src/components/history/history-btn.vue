48ecd84777a4992a0809c83ddaff85497b57f0cc
JANGY\江浙沪子笑 <2693538648@qq.com>
2021-04-24 11:10:08 +0800

    添加历史模块

---
+++ index/src/components/history/history-btn.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="history-btn">
+    <a href="#" class="btn">暂停记录历史</a>
+    <a href="#" class="btn cleanhsbtn">清空历史</a>
+  </div>
+</template>
+
+<script>
+export default {
+name: "history-btn"
+}
+</script>
+
+<style scoped>
+
+</style>
