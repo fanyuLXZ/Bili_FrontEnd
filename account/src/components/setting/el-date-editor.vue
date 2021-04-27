04fe62d44d112c69c0e40885253335b04dd1d151
fanyuLXZ <1554296667@qq.com>
2021-04-27 08:55:46 +0800

    添加el-picker-panel日期控件

---
+++ account/src/components/setting/el-date-editor.vue
@@ -0,0 +1,47 @@
+<template>
+  <z-toggle :is_show="isShow" @display_update="display_update" :transition_name="'el-zoom-in-top'">
+    <template v-slot:btn>
+      <div class="el-date-editor el-input el-date-editor--date" id="el-date-pick"><!---->
+        <i class="el-input__icon el-icon-date"></i>
+        <input autocomplete="off" placeholder="选择日期"  v-model=date readonly="readonly" type="text" rows="2" class="el-input__inner"><!----><!---->
+      </div>
+    </template>
+    <el-picker-panel v-model=date @onChange="onChange"/>
+  </z-toggle>
+</template>
+
+<script>
+import elPickerPanel from "g-public/components/el-picker-panel"
+import zToggle from "g-public/components/z-toggle"
+export default {
+  name: "el-date-editor",
+  model:{
+    prop:"birthday",
+    event:"change",
+  },
+  props:["birthday"],
+  data(){
+    return{
+      "isShow":false,
+      "date":this.birthday,
+    }
+  },
+  components:{
+    elPickerPanel,
+    zToggle
+  },
+  methods:{
+    display_update(isShow){
+      this.isShow=isShow
+    },
+    onChange(){
+      this.isShow=false
+      this.$emit('change',this.date)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- public/src/js/utils.js
+++ public/src/js/utils.js
@@ -712,3 +712,34 @@ export const getLimitString = (str, initLimt) => {
     return str
   }
 };
+
+export const convertToChinaNum = (num)=> {
+  var arr1 = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九'];
+  var arr2 = ['', '十', '百', '千', '万', '十', '百', '千', '亿', '十', '百', '千','万', '十', '百', '千','亿'];//可继续追加更高位转换值
+  if(!num || isNaN(num)){
+    return "零";
+  }
+  var english = num.toString().split("")
+  var result = "";
+  for (var i = 0; i < english.length; i++) {
+    var des_i = english.length - 1 - i;//倒序排列设值
+    result = arr2[i] + result;
+    var arr1_index = english[des_i];
+    result = arr1[arr1_index] + result;
+  }
+  //将【零千、零百】换成【零】 【十零】换成【十】
+  result = result.replace(/零(千|百|十)/g, '零').replace(/十零/g, '十');
+  //合并中间多个零为一个零
+  result = result.replace(/零+/g, '零');
+  //将【零亿】换成【亿】【零万】换成【万】
+  result = result.replace(/零亿/g, '亿').replace(/零万/g, '万');
+  //将【亿万】换成【亿】
+  result = result.replace(/亿万/g, '亿');
+  //移除末尾的零
+  result = result.replace(/零+$/, '')
+  //将【零一十】换成【零十】
+  //result = result.replace(/零一十/g, '零十');//貌似正规读法是零一十
+  //将【一十】换成【十】
+  result = result.replace(/^一十/g, '十');
+  return result;
+}
